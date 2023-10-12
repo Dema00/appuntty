4830296d264c7d2861144619e09b95650c4cdffc
Francesco De Marchi <dema199900@gmail.com>
2023-10-12 02:06:13 +0200

    Parser test with word

--- src/backend/node.rs
+++ src/backend/node.rs
@@ -1,22 +1,27 @@
+#[derive(Debug, PartialEq, Eq)]
 pub struct UUID<'p> {
     parent: &'p UUID<'p>,
     id: i32,
 }
 
-pub enum NodeContent<'p> {
+#[derive(Debug, PartialEq, Eq)]
+pub enum NodeContent <'p> {
     Text(String),
     Reference(UUID<'p>),
     Blob((String, Box<Node<'p>>)),
+    TempRef(Vec<u32>),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Node<'p> {
     parent: &'p Node<'p>,
     uuid: UUID<'p>,
-    cont: Vec<NodeContent<'p>>,
+    cont: Vec<NodeContent <'p>>,
     sons: Vec<Box<Node<'p>>>,
     prop: Vec<NodeProperty>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum NodeProperty {
     Color,
     Rbind,
--- src/backend/parser.rs
+++ src/backend/parser.rs
@@ -1,32 +1,73 @@
 extern crate nom;
 
-use super::node::{NodeContent};
+use super::node::NodeContent;
 
 use std::str::FromStr;
 
 use nom::{
-    bytes::complete::{tag, take, take_while, take_while_m_n},
-    character::{is_alphabetic, is_alphanumeric, is_digit, complete::digit1},
-    combinator::map_res,
-    sequence::{delimited, tuple},
-    IResult, multi::{separated_list1, many_till}, branch::alt,
+    branch::alt,
+    bytes::complete::{tag, take, take_till, take_until, take_while, take_while_m_n},
+    character::{
+        complete::char, complete::digit1, is_alphabetic, is_alphanumeric, is_digit, is_space,
+    },
+    combinator::{map, map_res},
+    multi::{fold_many0, many_till, separated_list1},
+    sequence::{delimited, preceded, tuple, terminated},
+    IResult, Parser,
 };
 
-struct Parser {
-    curr_id: u32,
-}
-
 type TempRefID = Vec<u32>;
-type Word = Vec<String>;
 
 pub fn parse_numbers(input: &str) -> IResult<&str, u32> {
     map_res(digit1, u32::from_str)(input)
 }
 
 fn uuid(input: &str) -> IResult<&str, TempRefID> {
-    let (input, _) = tag("#")(input)?;
-    let (input, temp_ref_id) = delimited(tag("("), separated_list1(tag("."),parse_numbers), tag(")"))(input)?;
-    Ok((input, temp_ref_id))
+    preceded(
+        tag("#"),
+        delimited(tag("("), separated_list1(tag("."), parse_numbers), tag(")")),
+    )
+    .parse(input)
+}
+
+#[rustfmt::skip]
+fn word(input: &str) -> IResult<&str, String> {
+    preceded(
+        tag(" "), 
+        map(
+            terminated(
+                take_until(" "),tag(" ")
+            ), 
+            |word| String::from(word))
+    )
+    .parse(input)
+}
+
+#[rustfmt::skip]
+fn node_content_fragment(input: &str) -> IResult<&str, NodeContent> {
+    alt(
+        (
+            map(uuid, |ref_id| NodeContent::TempRef(ref_id)),
+            map(word, |word| NodeContent::Text(word))
+        )
+    )
+    .parse(input)
+}
+
+fn node_content(input: &str) -> IResult<&str, Vec<NodeContent>> {
+    fold_many0(
+        // Our parser function– parses a single string fragment
+        node_content_fragment,
+        // Our init value, an empty string
+        Vec::new,
+        // Our folding function. For each fragment, append the fragment to the
+        // string
+        |mut contents: Vec<NodeContent>, fragment| {
+            contents.push(fragment);
+            contents
+        },
+    )
+    .parse(input)
 }
 
 /*fn next_node<'t, 'p, P: NodeProperty>(
@@ -40,11 +81,30 @@ fn uuid(input: &str) -> IResult<&str, TempRefID> {
 
 #[cfg(test)]
 mod tests {
-    use crate::backend::parser::uuid;
+    use crate::backend::{
+        node::NodeContent,
+        parser::{uuid, node_content},
+    };
 
     #[test]
     fn uuid_test() {
         let res = uuid("#(1.2.3)");
-        assert_eq!(res, Ok(("",vec![1,2,3])));
+        assert_eq!(res, Ok(("", vec![1, 2, 3])));
+    }
+
+    #[test]
+    fn node_ez_test() {
+        let res = node_content(" ciao #(1.2.3) eccomi ");
+        assert_eq!(
+            res,
+            Ok((
+                "",
+                vec![
+                    NodeContent::Text(String::from("ciao")),
+                    NodeContent::TempRef(vec![1, 2, 3]),
+                    NodeContent::Text(String::from("eccomi"))
+                ]
+            ))
+        );
     }
 }
